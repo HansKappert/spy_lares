89de2fd67a3aa3996ceb0b4ab236ad0caf8d04b8
Hans Kappert <hans.kappert@gmail.com>
2025-09-12 22:40:33 +0200

    better var name

--- ispy.py
+++ ispy.py
@@ -7,17 +7,18 @@ import agent
 
 load_dotenv()
 
-ip   =     os.getenv('ISPY_IP'  )
+ispy_api_ip   =     os.getenv('ISPY_IP'  )
 ispy_api_port = int(os.getenv('ISPY_API_PORT', 8090))
 
 async def main():
-    url = f"http://{ip}:{ispy_api_port}"
+    url = f"http://{ispy_api_ip}:{ispy_api_port}"
     ispy = agent.Agent(url)
 
     devices = await ispy.get_devices()
     # devices = await ispy.get_profiles()
     for device in devices:
         print(f"id={device.id}, name={device.name}")
+    ispy.arm()
     pass
 
 asyncio.run(main())
